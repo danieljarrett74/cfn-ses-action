520ecc3aab859a00baaf611382910f3c2ea42478
Daniel Jarrett <dan@carcloud.com>
2021-09-08 22:04:29 +1000

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 export AWS_ACCESS_KEY_ID=$1
 export AWS_SECRET_ACCESS_KEY=$2
 export AWS_DEFAULT_REGION=$3
-echo "$AWS_ACCESS_KEY_ID"
+echo "$1"
 
 aws --region us-east-1 \
 	cloudformation deploy --stack-name test-stack \
